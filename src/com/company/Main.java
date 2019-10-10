591fc6fdf8c4df9e722c0036645be288c8b95139
Tung Vu <tungld98vn@gmail.com>
2019-10-09 23:29:03 -0500

    add writeFile class

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("dit me may");
+    }
+}
